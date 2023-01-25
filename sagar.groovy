91bf79dd174bb7b4f88c4d0721ffcdcaecbf8ed2
sneha884 <nlanjewar128@gmail.com>
2023-01-25 16:48:42 +0530

    updated

--- sagar.groovy
+++ sagar.groovy
@@ -7,7 +7,7 @@ pipeline {
         {
                       sh '''  
                           sudo apt-get update -y
-                         sudo apt-get install git
+                         sudo apt-get install git -y
                       '''
                     git credentialsId: 'ubuntu', url: 'https://github.com/sneha884/stud.git'
         }
